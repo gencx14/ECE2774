611431da32c8fad5a77ffffca6b9d6c043581406
LRV14 <LRV14@pitt.edu>
2023-02-08 13:35:33 -0500

    Created Geometry file

---
+++ Geometry.py
@@ -0,0 +1,15 @@
+import math
+
+
+class Geometry:
+
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.Dab_x = abs(x1-x2)
+        self.Dab_y = abs(y1-y2)
+        self.Dbc_x = abs(x2 - x3)
+        self.Dbc_y = abs(y2-y3)
+        self.Dca_x = abs(x1-x3)
+        self.Dca_y = abs(y1-y3)
+        self.Dab = math.sqrt(self.Dab_x**2 + self.Dab_y**2)
+        self.Dbc = math.sqrt(self.Dbc_x**2 + self.Dbc_y**2)
+        self.Dca = math.sqrt(self.Dca_x**2 + self.Dca_y**2)
