175c32e599fe8cef7f2e8c71a306f6828d14d266
LRV14 <LRV14@pitt.edu>
2023-02-08 16:19:54 -0500

    Finished Transformer and TransmissionLine classes

--- Transformer.py
+++ Transformer.py
@@ -1,5 +1,14 @@
 from TransformerData import TransformerData
+import math
 
 
 class Transformer:
-    pass
+
+    j = math.sqrt(-1)
+
+    def __init__(self, name: str, data: TransformerData):
+        self.name = name
+        self.data = data
+        self.y = 1/(data.R + (Transformer.j*data.X))        # Admittance of transformer
+
+
--- TransmissionLine.py
+++ TransmissionLine.py
@@ -1,15 +1,21 @@
 from TransmissionLineData import TransmissionLineData
+import math
 
 
 class TransmissionLine:
+
+    p_base = 100       # target apparent power base is 100MVA
+    v_base = 230       # voltage base is 230kV
+    z_rated = v_base**2/p_base    # impedance base
+    y_rated = 1/z_rated     # admittance base
+    j = math.sqrt(-1)
 
     def __init__(self, bus1, bus2, length, data: TransmissionLineData):
         self.data = data
         self.bus1 = bus1
         self.bus2 = bus2
         self.length = length
-        self.R = data.R * self.length       # Resistance of line in Ohms
-        self.X = data.L * self.length * data.w       # Reactance of line in Ohms
-        self.B = data.C * self.length * data.w       # Shunt admittance of line
-
-        self.buses = [self.bus1, self.bus2]
+        self.R = data.R * self.length / TransmissionLine.z_rated      # Resistance of line in pu
+        self.X = data.L * self.length * data.w / TransmissionLine.z_rated     # Reactance of line in pu
+        self.B = data.C * self.length * data.w / TransmissionLine.y_rated      # Shunt admittance of line in pu
+        self.y = 1/(self.R + (TransmissionLine.j * self.X))     # Admittance of line
